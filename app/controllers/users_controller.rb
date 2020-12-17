3440f09bfbcb7b867bf98b0a8fd009db5b6a572d
nami-s <na.s1222.10feettt@gmail.com>
2020-12-17 03:21:25 +0000

    [Add]各ページにユーザプロフィールと新規投稿フォームの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def create
@@ -25,11 +27,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image_id)
-  end  
+  end
 
 end
