c2fdbd83d5294813bc5672da842013ff4a48b13c
nami-s <na.s1222.10feettt@gmail.com>
2020-12-19 12:40:43 +0000

    [Finish]RSpecテスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  
-  before_action :baria_user, only: [:edit, :destroy, :update]
-
 
   def index
     @books = Book.all
@@ -30,6 +27,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
@@ -51,12 +51,6 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
-  end
-  
-  def baria_user
-    unless Book.find(params[:id]).user.id.to_i == current_user.id
-      redirect_to books_path(current_user)
-    end
   end
 
 end
