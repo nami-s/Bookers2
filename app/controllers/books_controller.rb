4fd0b2cce87fb5d4be4a8c5d10901ca5e15d9c3a
nami-s <na.s1222.10feettt@gmail.com>
2020-12-19 08:02:23 +0000

    [Add]サクセスメッセージ の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,21 @@
 class BooksController < ApplicationController
-  
+
 
   def index
     @books = Book.all
     @book = Book.new
     @user = User.find(current_user.id)
-    
+
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
-    redirect_to book_path(book.id), notice: 'You have created book successfully..'
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'You have created book successfully..'
     else
+      @user = current_user
       @books = Book.all
-      @book = Book.new
-      @user = User.find(current_user.id)
       render 'index'
     end
   end
@@ -32,11 +31,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id), notice: 'You have updated book successfully'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'You have updated book successfully'
     else
-      @book = Book.find(params[:id])
       render 'edit'
     end
   end
