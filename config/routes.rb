49a156be0d57caa9b557e438a7c16cab32d4ef73
nami-s <na.s1222.10feettt@gmail.com>
2020-12-22 07:35:20 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   root to: 'homes#top'
   #get 'books' => 'books#index'
   get 'home/about' => 'homes#about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_114134) do
+ActiveRecord::Schema.define(version: 2020_12_22_070755) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_12_16_114134) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
